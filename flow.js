9154bcfb2644439b900b18de8b0432a9ff6499d4
schurterb <12720208+schurterb@users.noreply.github.com>
2023-11-14 17:10:40 -0500

    debug

--- flow.js
+++ flow.js
@@ -118,8 +118,9 @@ function buildFlowSystem() {
     fs.cpSync('src/all', 'build/www/js', {recursive: true});
     // fs.copyFileSync('src/web/*', 'build/www/js');
     fs.cpSync('src/web', 'build/www/js', {recursive: true});
+    fs.execSync('cp -rf node_modules/flow-client/dist/* build/www/js/')
     // fs.copyFileSync('node_modules/flow-client/dist/*', 'build/www/js/');
-    fs.cpSync('node_modules/flow-client/dist', 'build/www/js', {recursive: true});
+    // fs.cpSync('node_modules/flow-client/dist', 'build/www/js', {recursive: true});
 
     // Finish
     console.log('Done');
