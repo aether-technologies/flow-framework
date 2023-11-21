7767ab2122b4a2ead026adf21d6b0efd7f1558ee
schurterb <12720208+schurterb@users.noreply.github.com>
2023-11-20 20:16:31 -0500

    Updated default Flow Framework.

---
+++ src/web/flow.mjs
@@ -0,0 +1,5 @@
+import FlowNode, { Flow, FlowMessage, FlowConnector, FlowRouter, FlowManager, FlowMonitor } from './flow.bundle.mjs';
+
+//TODO: Somewhere in here modify them 'tools'
+
+export { FlowNode, Flow, FlowMessage, FlowConnector, FlowRouter, FlowManager, FlowMonitor };
